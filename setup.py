2d8a9c1d48e4a659402259d9ade8dd413da71724
Ubuntu <ubuntu@ip-172-31-47-59.us-west-2.compute.internal>
2022-03-27 23:37:51 +0000

    before heroku

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='End to end ML pipeline using multiple open source tools',
+    author='kaushik_dasroy',
+    license='MIT',
+)
